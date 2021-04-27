0d42d269a1d265edb7e5d7897dd08df95b405425
debian <debian@10>
2021-04-27 17:03:40 -0400

    last_digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,11 +5,10 @@ if number < 0:
     mod = ((number * -1) % 10) * -1
 else:
     mod = number % 10
-
 str = "Last digit of {:d} is {:d} and {}"
 if mod == 0:
-    print(str .format(number, mod,  'and is 0'))
+    print(str .format(number, mod,  'is 0'))
 elif mod > 5:
-    print(str .format(number, mod, 'and is greater than 5'))
+    print(str .format(number, mod, 'is greater than 5'))
 elif mod < 6:
-    print(str .format(number, mod, 'and is less than 6 and not 0'))
+    print(str .format(number, mod, 'is less than 6 and not 0'))
