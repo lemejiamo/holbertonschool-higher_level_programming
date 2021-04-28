1856a428ee6bf5bf19d7d303a82e144f6fb7e4ea
debian <debian@10>
2021-04-27 22:52:37 -0400

    print a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    length = len(str)
+    i = 0
+    while i < length:
+        c = ord(str[i])
+        if c >= 97 and c <= 122:
+            c = c - 32
+        print("{:c}".format(c), end='')
+        i += 1
+
+    print("\n")
