e684d29e64c2d4acb53f2e4bfa9196dce2407c10
debian <debian@10>
2021-04-28 01:36:16 -0400

    add number to list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - insert number in a sorted linked list 
+ * @head: pointer to head of linked list
+ * @number: number to insert in linkedlist
+ * Return: pointer to node added 
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	/* verify if data is valid */
+	if (head == NULL)
+		return (NULL);
+
+	listint_t *current = NULL;
+	listint_t *new_node = NULL;
+	listint_t *past_node = NULL;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = number;
+	current = *head;
+	past_node = *head;
+
+	if (current->n < number)
+		current = current->next;
+
+	for (; current->n < number;)
+	{
+		past_node = past_node->next;
+		current = current->next;
+	}
+
+	new_node->next = current;
+	past_node->next = new_node;
+
+	return (new_node);
+}
