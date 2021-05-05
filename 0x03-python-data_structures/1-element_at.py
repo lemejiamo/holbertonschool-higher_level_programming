2183c104ca7fbf0c006f879fa1ae866cba89fa4f
lemejiamo <luismejia69@gmail.com>
2021-05-05 16:17:54 -0400

    print secure element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if (idx < 0):
-        return NULL
+        return None
     if len(my_list) < idx:
         return None
     return (my_list[idx])
