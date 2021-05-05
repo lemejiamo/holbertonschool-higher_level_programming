496bf16ac8c5657b6e544c91fcc5f987713cead2
lemejiamo <luismejia69@gmail.com>
2021-05-05 17:25:40 -0400

    create a new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if (idx < 0):
+        return
+    if (len(my_list) - 1) < idx:
+        return
     if not my_list:
         return
     # create a empty list
