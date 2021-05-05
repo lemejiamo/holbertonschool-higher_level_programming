6664368b7003627e4daa2cd73b1f4c32c722943d
lemejiamo <luismejia69@gmail.com>
2021-05-05 17:34:31 -0400

    create a new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,12 +2,12 @@
 def new_in_list(my_list, idx, element):
     if (idx < 0) or (len(my_list) - 1) < idx:
         return my_list
-    #if (len(my_list) - 1) < idx:
-    #   return my_list
     if not my_list:
         return
+
     # create a empty list
     copy_list = []
+
     # add the elemtes from my_list to empty list
     copy_list.extend(my_list)
     copy_list[idx] = element
