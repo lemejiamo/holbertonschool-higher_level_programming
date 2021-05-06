7270904d90f52df4c976d0268265b08a5a9bd69d
lemejiamo <luismejia69@gmail.com>
2021-05-05 20:27:05 -0400

    remove firts cin a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     if not my_string:
         return
-    
-    new_string = my_string.translate({ord(i):'' for i in "Cc"})
+
+    new_string = my_string.translate({ord(i): '' for i in "Cc"})
     return (new_string)
