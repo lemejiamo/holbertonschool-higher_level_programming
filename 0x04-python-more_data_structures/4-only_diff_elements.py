c739007d61de16a6d32624f8eb4b6122bc465431
lemejiamo <luismejia69@gmail.com>
2021-05-08 19:44:35 -0400

    only different elements

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    uniques = list(set_1) + list(set_2)
-    uniques = list(dict.fromkeys(uniques))
-    return uniques
+    # build a list with the set's
+    unified_list = list(set_1) + list(set_2)
+# revisar cada posicion contra todas las demas
+    dup = 0
+    i = 0
+# cicle to count repetition form itmes in a unified list
+    for i in unified_list:
+        j = 0
+        dup = 0
+        # verify the item with all index possition in the list
+        for j in range(len(unified_list)):
+            if i == unified_list[j]:
+                dup += 1
+        # if ites is more for 1 time in list
+        if dup > 1:
+            idx = 0
+            # cicle to remove repeated items
+            while idx < dup:
+                unified_list.remove(i)
+                idx += 1
+    return unified_list
