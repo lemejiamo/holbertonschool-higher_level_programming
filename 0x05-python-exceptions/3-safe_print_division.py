06bba31d2dedaaccd5769f156f0831fef1b9bb64
lemejiamo <luismejia69@gmail.com>
2021-05-17 17:24:21 -0500

    Safe print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,5 +4,5 @@ def safe_print_division(a, b):
         div = a / b
         print("Inside result: {:.1f}".format(div))
         return div
-    except ZeroDivisionError:
+    except:
         print("Inside result: None")
