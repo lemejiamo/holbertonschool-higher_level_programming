67bf03c4b3180a358f56eb7a08a5163272c8f977
lemejiamo <luismejia69@gmail.com>
2021-05-17 16:40:30 -0500

    Safe print integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = j = 0
+    while (j < x):
+        try:
+            int(my_list[j])
+            print("{:d}".format(my_list[j]), end="")
+            j += 1
+            i += 1
+        except :
+            j += 1
+    print()
+    return i
