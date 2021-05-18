cc0ebc6749606d52edebd97cfb04a95ee94c5437
lemejiamo <luismejia69@gmail.com>
2021-05-18 17:34:16 -0500

    1-square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,12 @@
 
 
 class Square():
+    """Class square
+        Define class square
+    """
     def __init__(self, size):
+        """This function
+            pass size
+            how attribute
+        """
         self.__size = size
