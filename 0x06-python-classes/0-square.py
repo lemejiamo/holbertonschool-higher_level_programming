941f23beb503b865784d4eb81987bbafd590d760
lemejiamo <luismejia69@gmail.com>
2021-05-18 17:42:03 -0500

    0-square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-
+"""Square void
+    Define Class square
+"""
 
 class Square():
     """void function
         no argument requierd
     """
     def funtion(self):
+        """void function
+            no argument requierd
+        """
         pass
