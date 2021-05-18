cea33c19e53cac247a1109bff202798d0587f171
lemejiamo <luismejia69@gmail.com>
2021-05-18 17:32:43 -0500

    1-square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, _Square__size):
-        self._Square__size = _Square__size
+"""Square with size
+    that define size of square
+"""
+
+
+class Square():
+    def __init__(self, size):
+        self.__size = size
