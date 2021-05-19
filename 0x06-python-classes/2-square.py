afc4dbce61ab1648bb978261e03215c7665a5efb
lemejiamo <luismejia69@gmail.com>
2021-05-18 19:10:27 -0500

    2-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,9 +9,10 @@ class Square():
     def __init__(self, size=0):
         """ constructor
             to create a square
+
         """
-        self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
