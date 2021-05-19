f2230ebb1ac78e7b0c5aa53417fcb2c27605d2be
lemejiamo <luismejia69@gmail.com>
2021-05-18 19:03:52 -0500

    2-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,12 +6,11 @@ class Square():
        Define class square
     """
     def __init__(self, size=0):
-        """This function
-            pass size
-            how attribute
+        """This function pass size
+            as attribute and validate it
         """
+        self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
