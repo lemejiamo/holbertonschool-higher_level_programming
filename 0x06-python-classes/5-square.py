ad8912754e2a317fd0365516eaaa4b57008d0592
lemejiamo <luismejia69@gmail.com>
2021-05-18 20:45:26 -0500

    5-square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Square
+   Learning attributes works into a classes
+   and objects
+"""
+
+
+class Square():
+    """Class square define a square
+
+       __init__ initialization of private instance
+    """
+    def __init__(self, size=0):
+        """ constructor
+            to create a square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+            calculate a area of square
+        """
+        self.area = (self.__size * self.__size)
+        return self.area
+
+    def my_print(self):
+        for i in range(self.size):
+            i = 0
+            while i < self.size:
+                print("#", end="")
+                i += 1
+            print()
