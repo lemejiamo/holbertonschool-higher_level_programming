5e032410e423303e7dc3db92dbf7273fe34a857c
lemejiamo <luismejia69@gmail.com>
2021-05-31 23:16:44 -0500

    add item to a list using json files

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" script to add elements to a python list
+and then save the them to a file
+"""
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+list = []
+list = load_from_json_file("add_item.json")
+list.extend(sys.argv[1:])
+save_to_json_file(list, "add_item.json")
