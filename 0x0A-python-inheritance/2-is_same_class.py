47a4a10aec9502716086a4e5993d8cb9cbc45367
lemejiamo <luismejia69@gmail.com>
2021-06-02 18:09:22 -0500

    learning classes

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@ belongs to a given class """
 
 def is_same_class(obj, a_class):
     """verify if object belongs to given class """
-    
+
     if type(obj) is a_class:
         return True
     else:
