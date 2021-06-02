2209e1bfc3f6de8e3828e4956bd565f241e14cf8
lemejiamo <luismejia69@gmail.com>
2021-06-02 15:51:44 -0500

    learning to use sort builtin

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function to print list sorted """
+
+class MyList(list):
+    """ class make task with list"""
+
+    def print_sorted(self):
+        """print sorted method"""
+        print(sorted(self))
