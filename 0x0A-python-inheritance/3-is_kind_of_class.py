404adc97d07748fd07a340e3d565b173c015bc39
lemejiamo <luismejia69@gmail.com>
2021-06-02 18:14:00 -0500

    is a kind of class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,9 +5,7 @@
 def is_kind_of_class(obj, a_class):
     """ verify if object belongs to given class """
 
-    if type(obj) is a_class:
-        return True
-    elif isinstance (obj, a_class): 
+    if isinstance (obj, a_class): 
         return True
     else:
         return False
