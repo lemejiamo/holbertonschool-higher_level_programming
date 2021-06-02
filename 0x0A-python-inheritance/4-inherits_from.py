aaa4a10c2292c172b807c5fd942c6c2468d9b0e1
lemejiamo <luismejia69@gmail.com>
2021-06-02 18:22:08 -0500

    inherits from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """ object is kind of class"""
 
 
 def inherits_from(obj, a_class):
+    """ verify if a object is inherit from a given class """
+
     if type(obj) == a_class:
         return False
+
     if isinstance(obj, a_class):
         return True
+
     else:
         return False
