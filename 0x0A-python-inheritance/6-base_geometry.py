1866300b709ad47c201208669a58566b4a945c59
lemejiamo <luismejia69@gmail.com>
2021-06-02 18:42:56 -0500

    base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class to learn python project 0x0A Holberton School """
+
+
+class BaseGeometry():
+    """ a class to develop"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
