02c19b7431dace305ad4f0d6936a90fb62d2044d
lemejiamo <luismejia69@gmail.com>
2021-06-02 16:34:09 -0500

    learning to verify a class of the object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" verify if a object
+belongs to a given class """
+
+
+def is_same_class(obj, a_class):
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
