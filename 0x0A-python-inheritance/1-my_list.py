d5089025b5f92abf025d8be7407390d3baab3ed6
lemejiamo <luismejia69@gmail.com>
2021-06-02 22:43:05 -0500

    print sorted list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" function to print list sorted """
+""" function to print list sorted."""
 
 
 class MyList(list):
-    """ class make task with list"""
+    """ class make task with list."""
 
     def print_sorted(self):
-        """print sorted method"""
+        """print sorted method."""
         print(sorted(self))
