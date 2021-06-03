44c201e1f0be29997b1e8b24eba8f9d0ebb8f6ff
lemejiamo <luismejia69@gmail.com>
2021-06-02 19:11:46 -0500

    rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" class to learn python project 0x0A Holberton School """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ define a class Rectangle inherits from  BaseGeometry"""
+
+    def __init__(self, width, height):
+        """ constructor
+
+        Attributes:
+            width: Private
+            height: Private
+        """
+
+        self.__width = width
+        self.__height = height
