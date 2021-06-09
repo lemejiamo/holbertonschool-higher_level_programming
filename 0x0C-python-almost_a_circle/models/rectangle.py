528fae467fe39f8a1088d8c83deba3ea511659a5
lemejiamo <luismejia69@gmail.com>
2021-06-08 19:49:13 -0500

    fixed minor bug in integer validator function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -55,7 +55,9 @@ class Rectangle(Base):
 
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        if value < 0:
+        if value == 0 and name == "y" or name == "x":
+            return
+        if value <= 0:
             if name == "y" or name == "x":
                 raise ValueError("{} must be >= 0".format(name))
             raise ValueError("{} must be > 0".format(name))
