aa97db8f047658a1d3e020f02682d957773a84fb
lemejiamo <luismejia69@gmail.com>
2021-07-13 21:51:50 -0500

    genre id all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- all shows by genre id
 SELECT tv_shows.title, tsg.genre_id
 	FROM tv_shows
-	JOIN tv_show_genres AS tsg
+	LEFT JOIN tv_show_genres AS tsg
 	ON tsg.show_id=tv_shows.id
 	ORDER BY tv_shows.title, tsg.genre_id ASC;
 
