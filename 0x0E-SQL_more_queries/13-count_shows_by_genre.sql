c6f70fc34356681840ca8b50b2b63301786b99f0
lemejiamo <luismejia69@gmail.com>
2021-07-13 22:07:47 -0500

    count shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all genres.
+SELECT tg.name AS genre,
+       COUNT(shgr.genre_id) AS number_of_shows FROM tv_genres AS tg
+       JOIN tv_show_genres AS tsg
+       ON tg.id=tsg.genre_id
+       GROUP BY tsg.genre_id
+       ORDER BY number_of_shows DESC;
