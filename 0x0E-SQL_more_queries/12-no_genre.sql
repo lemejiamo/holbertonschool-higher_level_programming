8644937c6123f93b4384edc050b5f68d9b27c326
lemejiamo <luismejia69@gmail.com>
2021-07-13 22:02:58 -0500

    no genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- list all show without genre
+SELECT tv_shows.title, tsg.genre_id FROM tv_shows
+	LEFT JOIN tv_show_genres AS tsg
+	ON tv_shows.id = tsg.show_id
+	WHERE tsg.genre_id IS NULL
+	ORDER BY tv_shows.title, tsg.genre_id ASC;
