5241895c71a547d073bdf274a4adccfd3113d6c5
lemejiamo <luismejia69@gmail.com>
2021-08-03 18:51:46 -0500

    function print added

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -14,7 +14,7 @@ class Rectangle {
     for (let i = 0; i < this.height; i++) {
       let line = '';
       for (let j = 0; j < this.width; j++) {
-        line = `${line} X`;
+        line = `${line}X`;
       }
       console.log(line);
     }
