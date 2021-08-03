c5fa736cb67fe51478fad3f9acb3ee1ff1526236
lemejiamo <luismejia69@gmail.com>
2021-08-03 18:46:26 -0500

    function print added

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w < 1 || h < 1 || w === undefined || h === undefined) {
+      return;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line = `${line} x`;
+      }
+      console.log(line);
+    }
+  }
+}
+
+module.exports = Rectangle;
