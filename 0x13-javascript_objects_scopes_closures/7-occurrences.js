11678d30e54c27fa19f0ac7581bf80b00c11bd86
lemejiamo <luismejia69@gmail.com>
2021-08-03 23:04:16 -0500

    occurrences function

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
 
-  let match = searchElement;
+  const match = searchElement;
   let count = 0;
 
   for (let i = 0; i < list.length; i++) {
