13738e5e77d5f7ae63867fd7a6757e1de53db53a
lemejiamo <luismejia69@gmail.com>
2021-08-03 22:28:04 -0500

    added charPrint function to class square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Square_old = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Square_old {
   charPrint (c) {
     if (typeof (c) === 'undefined') {
       c = 'X';
