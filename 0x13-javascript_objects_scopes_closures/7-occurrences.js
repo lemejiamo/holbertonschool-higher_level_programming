1686e3b2564e133d935a613892648deda996dac5
lemejiamo <luismejia69@gmail.com>
2021-08-03 23:30:45 -0500

    semistandar corrected

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-
   const match = searchElement;
   let count = 0;
 
   for (let i = 0; i < list.length; i++) {
     if (match === list[i]) {
-        count += 1;
+      count += 1;
     }
   }
   return count;
-}
+};
