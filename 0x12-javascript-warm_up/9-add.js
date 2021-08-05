5496feaec720a02282659408fc469736f6832d09
lemejiamo <luismejia69@gmail.com>
2021-08-04 23:31:56 -0500

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-function add(a, b) {
+function add (a, b) {
   return (a + b);
 }
 
 const argv = process.argv;
 const numOne = parseInt(argv[2]);
 const numTwo = parseInt(argv[3]);
-
 
 console.log(add(numOne, numTwo));
