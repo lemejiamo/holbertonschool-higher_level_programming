85e24d0837327212e103bcc0779f62e6f6e03a36
lemejiamo <luismejia69@gmail.com>
2021-08-07 18:58:19 -0500

    pep8  correction

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
+"""Filter States"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-	username = argv[1]
-	password = argv[2]
-	database_name = argv[3]
-	state = argv[4]
-	database = MySQLdb.connect(host="localhost",
-						 port=3306,
-						 user=username,
-						 passwd=password,
-						 db=database_name)
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    state = argv[4]
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=username,
+                               passwd=password,
+                               db=database_name)
 
-	query = "SELECT states.id, name FROM states where name='" + state + "' ORDER BY states.id ASC;"
-	cursor = database.cursor()
-	cursor.execute(query)
-	rows = cursor.fetchall()
+    query = """
+    SELECT states.id,
+    name FROM states
+    where name = '" + state + "' ORDER BY states.id ASC;
+    """
 
-	for row in rows:
-	   print(row)
+    cursor = database.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
 
-	cursor.close()
-	database.close()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    database.close()
