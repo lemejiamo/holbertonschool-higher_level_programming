e922f3759ce1a80f139551ac66d667e1495c9112
lemejiamo <luismejia69@gmail.com>
2021-08-07 19:17:33 -0500

    pep8 correction

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
+"""cities by state"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-	username = argv[1]
-	password = argv[2]
-	database_name = argv[3]
-	database = MySQLdb.connect(host="localhost",
-						 port=3306,
-						 user=username,
-						 passwd=password,
-						 db=database_name)
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=username,
+                               passwd=password,
+                               db=database_name)
 
-	cursor = database.cursor()
+    cursor = database.cursor()
+    cursor.execute("""
+    SELECT cities.id, name
+    FROM cities
+    ORDER BY cities.id ASC;
+    """)
 
-	cursor.execute("""
-		SELECT cities.id, name
-		FROM cities
-		ORDER BY cities.id ASC;
-    	""")
-
-	rows = cursor.fetchall()
-
-	for row in rows:
-	   print(row)
-
-	cursor.close()
-	database.close()
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    database.close()
