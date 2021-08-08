f49c19476ad65d5fcc57de82d76883eaed7f79c0
lemejiamo <luismejia69@gmail.com>
2021-08-08 11:26:49 -0500

    to list states thta contains 'a' letter

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""all states via SQLAlchemy"""
+
+import sys
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, passwd, database),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter(State.name.like('%a%'))
+    records = query.all()
+
+    for record in records:
+        print("{:d}: {:s}".format(record.id, record.name))
