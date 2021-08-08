155d55c83a3f164452d5e172220222a65e96a57f
lemejiamo <luismejia69@gmail.com>
2021-08-08 11:20:14 -0500

    fetch first state with SQLAlchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,7 +2,7 @@
 """all states via SQLAlchemy"""
 
 import sys
-from model_state import Base, State
+from model_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
