6ac6d4a550a3a98ed367c08ecab97998d0d8f3e0
lemejiamo <luismejia69@gmail.com>
2021-08-08 23:39:55 -0500

    model city

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -11,7 +11,20 @@ if __name__ == "__main__":
     passwd = sys.argv[2]
     database = sys.argv[3]
     state = sys.argv[4]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, passwd, database), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, passwd, database),
+                           pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    query = session.query(State.name, City.id, City.name).\
+        join(City, State.id == City.state_id).\
+        order_by(City.id)
+
+    records = query.all()
+
+    for record in records:
+        print("{:s}: ({:d}) {:s}".format(*record))
+
+    session.close()
