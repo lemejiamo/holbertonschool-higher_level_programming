f2c40f48b82a237e8ec7fa9ac33b641a48ddb581
lemejiamo <luismejia69@gmail.com>
2021-08-19 23:59:58 -0500

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # catch me if you can
-curl -sL  $1 -X PUT -d "user_id=98" -H "origin: HolbertonSchool"
+curl -sL  0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "origin: HolbertonSchool"
