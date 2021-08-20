ca098a4bade99411b4d011739bd296de8a120131
lemejiamo <luismejia69@gmail.com>
2021-08-19 20:37:51 -0500

    Get the body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Display the size of the body of the response
 
-REQUEST_TO=$1
-curl -sI "$REQUEST_TO" | awk '/Content-Length/ {print $2}'
+curl -sI "$1" | awk '/Content-Length/ {print $2}'
