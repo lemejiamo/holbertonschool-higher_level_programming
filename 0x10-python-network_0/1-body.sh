2280fdf6796de542a1caedeba2a7913a56226de3
lemejiamo <luismejia69@gmail.com>
2021-08-19 21:29:02 -0500

    erase blank lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Displays the body of the request url
-
 curl -sL -X GET $1
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #lists all methods accepted by the server
-
 curl -si $1 | grep "Allow:" | cut -d ' ' -f2-
